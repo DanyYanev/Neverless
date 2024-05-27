8a0bc3a8d2f45ed2e1109fff4df474f58e39df8f
DanyYanev <yanev89@gmail.com>
2024-05-27 22:54:06 +0300

    Working server

---
+++ src/main/scala/TransactionServer.scala
@@ -0,0 +1,31 @@
+import account.storage.AccountStorageImpl
+import cats.effect.{IO, IOApp}
+import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.implicits._
+import org.http4s.server.Router
+import transaction.controller.TransactionController
+import transaction.service.TransactionServiceImpl
+import transaction.storage.TransactionStorageImpl
+import withdrawal.java.WithdrawalServiceStub
+import withdrawal.scala.WithdrawalServiceImpl
+
+object TransactionServer extends IOApp.Simple {
+  override def run: IO[Unit] = {
+    val accountStorage = new AccountStorageImpl()
+    val transactionStorage = new TransactionStorageImpl()
+    val withdrawalService = new WithdrawalServiceImpl(new WithdrawalServiceStub())
+
+    val transactionService = new TransactionServiceImpl(withdrawalService, accountStorage, transactionStorage)
+
+    val transactionController = new TransactionController(transactionService, java.time.Clock.systemUTC())
+
+    val httpApp = Router("/api" -> transactionController.routes).orNotFound
+
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+  }
+}
